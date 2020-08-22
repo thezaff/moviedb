17093f464c4d77af33b64d15c3fa12461d2ff450
thezaff <zaffenergizante@gmail.com>
2020-08-22 14:54:52 +0500

    feat/services: fetch seasons

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -6,10 +6,6 @@ export class AnalyticsService {
   constructor(private _httpService: HttpService) {}
 
   getPopularSeries(): any {
-    return this._httpService
-      .get(
-        'http://api.themoviedb.org/3/movie/550?api_key=dd1b0143dd84aea4692c8b3f0045b050',
-      )
-      .pipe(map(response => response.data));
+    return 'analytics';
   }
 }
--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -1,27 +1,27 @@
 import { Injectable, HttpService } from '@nestjs/common';
-import { map, switchMap } from 'rxjs/operators';
+import { scan, map, switchMap } from 'rxjs/operators';
 import { forkJoin } from 'rxjs';
+
+const key = 'dd1b0143dd84aea4692c8b3f0045b050';
+const url = 'https://api.themoviedb.org/3/tv';
+const lang = 'language=en-US';
 
 @Injectable()
 export class EpisodesService {
-  constructor(private _httpService: HttpService) { }
+  constructor(private _httpService: HttpService) {}
 
   getTopEpisodes(id: number): any {
+    const getSeason = seasonNumber =>
+      this._httpService
+        .get(`${url}/${id}/season/${seasonNumber}?api_key=${key}&${lang}`)
+        .pipe(map(i => i.data));
+
     return this._httpService
-      .get(
-        `https://api.themoviedb.org/3/tv/${id}?api_key=dd1b0143dd84aea4692c8b3f0045b050&language=en-US`,
-      )
+      .get(`${url}/${id}?api_key=${key}&${lang}`)
       .pipe(
-        switchMap(result =>
-          forkJoin([
-            this._httpService.get(
-              `https://api.themoviedb.org/3/tv/${id}/season/${1}?api_key=dd1b0143dd84aea4692c8b3f0045b050&language=en-US`,
-            ),
-            this._httpService.get(
-              `https://api.themoviedb.org/3/tv/${id}/season/${2}?api_key=dd1b0143dd84aea4692c8b3f0045b050&language=en-US`,
-            ),
-          ]),
-        )
+        switchMap(({ data }) =>
+          forkJoin(data.seasons.map((season, idx) => getSeason(idx))),
+        ),
       );
   }
 }
